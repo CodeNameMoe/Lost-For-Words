ac0fe21fcdd49c87678761e514061e93e258465f
Mohamed Ali <newmoeali@gmail.com>
2023-01-18 13:18:32 +0000

    completed book form input

--- src/components/searchForm/SearchForm.jsx
+++ src/components/searchForm/SearchForm.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
-import "./SearchForm.css";
+import "../searchForm/searchForm.css";
 
 function SearchForm() {
-  return <div className=""></div>;
+  return (
+    <div className="search-form">
+      <div className="container">
+        <div className="search-form-content">
+          <form className="search-form">
+            <div className="search-form-elem flex flex-sb bg-white">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Where The Wild Things Are..."
+              />
+              <button type="submit" className="flex flex-c">
+                <FaSearch className="text-purple" size={35} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SearchForm;

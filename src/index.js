44d5ab3ce7c559f4df0f61bbd69c9e8f662a1aa6
Mohamed Ali <newmoeali@gmail.com>
2023-01-18 14:46:49 +0000

    completed fetch api logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AppProvider } from "./context";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
@@ -9,13 +10,15 @@ import BookDetails from "./components/BookDetails/BookDetails";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}>
-        <Route path="about" element={<About />} />
-        <Route path="book" element={<BookList />} />
-        <Route path="/book/:id" element={<BookDetails />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="about" element={<About />} />
+          <Route path="book" element={<BookList />} />
+          <Route path="/book/:id" element={<BookDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
 );
